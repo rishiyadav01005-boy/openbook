c341e150c900662359c3dc2555beecb8f33e3ff2
Rishi Yadav <rishiyadav01005@gmail.com>
2026-01-13 22:40:16 +0530

    force update backend and frontend

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# UPDATED VERSION 
 from flask import Flask, render_template, request, redirect, url_for, session, send_from_directory
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
